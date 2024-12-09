4396b81f69bc7a0fee9f0fbfc9c093562422755d
ahmetyusufbirdir03 <168556979+ahmetyusufbirdir03@users.noreply.github.com>
2024-12-09 16:03:45 +0300

    menu_final_commit

---
+++ Assets/Scripts/UI_Manager_sc.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.UI;
+using TMPro;
+using Unity.VisualScripting;
+
+public class UI_Manager_sc : MonoBehaviour
+{
+    [SerializeField] GameObject pause_menu;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
